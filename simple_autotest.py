63f0e67c651d30c4815b39e0b4215415242d9123
DuckTheDir <dirochik@gmail.com>
2024-12-01 10:54:41 +0300

    Tests from 01.12.2024 , 10:51

--- simple_autotest.py
+++
@@ -1,7 +0,0 @@
-from functions import salary, hello_who
-
-assert hello_who("Max") == "Hello,Max", "hello_who error:1"
-assert hello_who("Leo") == "Hello,Leo", "hello_who error:2"
-assert hello_who("Nikita") == "Hello,Nikita", "hello_who error:3"
-assert salary(2,1) == 4 , "salary error:1"
-assert salary(2,2) == 8, "salary error:2"
